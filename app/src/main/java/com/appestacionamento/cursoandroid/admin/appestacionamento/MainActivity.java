65a886c33b41469582f20ea02af7061c53981c45
BrunoAsnt <bruno20152013@gmail.com>
2017-06-15 17:45:24 -0300

    json
    json

--- app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/MainActivity.java
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/MainActivity.java
@@ -3,6 +3,8 @@ package com.appestacionamento.cursoandroid.admin.appestacionamento;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Teste
+
     }
 }
