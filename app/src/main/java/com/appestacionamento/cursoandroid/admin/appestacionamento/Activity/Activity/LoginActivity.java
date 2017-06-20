9f169934958d5878603b5578d2c6ee7d98c96971
FlavioRoberto <flaviolive3@hotmail.com>
2017-06-20 09:24:28 -0300

    Estrutura android MvP

---
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Activity/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
