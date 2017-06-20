def5fbec74f3c64cc8a06ab00a8c55d19650f027
FlavioRoberto <flaviolive3@hotmail.com>
2017-06-20 09:00:21 -0300

    Estrutura android MvP

--- app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/model/Usuario.java
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/model/Usuario.java
@@ -3,72 +3,42 @@ package com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.Mode
 
 import com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.Application.configuracaoFirebase;
 import com.google.firebase.database.DatabaseReference;
-<<<<<<< Updated upstream
+import com.google.firebase.database.Exclude;
+
 
 public class Usuario {
     private String nome, uid, telefone, email, tipo;
-    private final String senha = "200200";
+    private  String senha ;
     private boolean status;
     private int cpf;
     private DatabaseReference firebaseReferences;
 
-    public Usuario(String nome, String uid, String telefone, String email, boolean status, int cpf, String tipo) {
-        this.nome = nome;
-        this.uid = uid;
-        this.telefone = telefone;
-        this.email = email;
-        this.status = status;
-        this.cpf = cpf;
-        this.tipo = tipo;
+
+    public Usuario() {
         firebaseReferences = configuracaoFirebase.getFirebase();
     }
 
-    public void create(){
-        firebaseReferences.child("users").child(getUid()).setValue(this);
-    }
-
-=======
-import com.google.firebase.database.Exclude;
-
-public class Usuario {
-    private String nome, uid,senha,telefone,email,tipo;
-    private boolean status;
-    private int cpf;
-
-    private DatabaseReference firebaseReference;
-
-
-    public Usuario(){
-        firebaseReference = configuracaoFirebase.getFirebase();
+    public void Create() {
+        firebaseReferences.child("Usuarios").child(getUid()).setValue(this);
     }
 
 
-    public void Create(){
-        firebaseReference.child("Usuarios").child(getUid()).setValue(this);
-    }
-
-
-
->>>>>>> Stashed changes
     public String getNome() {
         return nome;
     }
 
-<<<<<<< Updated upstream
-=======
+
     public void setNome(String nome) {
         this.nome = nome;
     }
 
     //@Exclude nao upa pro banco o campo
     @Exclude
->>>>>>> Stashed changes
     public String getUid() {
         return uid;
     }
 
-<<<<<<< Updated upstream
-=======
+
     public void setUid(String uid) {
         this.uid = uid;
     }
@@ -82,51 +52,44 @@ public class Usuario {
         this.senha = senha;
     }
 
->>>>>>> Stashed changes
+
     public String getTelefone() {
         return telefone;
     }
 
-<<<<<<< Updated upstream
-=======
+
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
 
     @Exclude
->>>>>>> Stashed changes
     public String getEmail() {
         return email;
     }
 
-<<<<<<< Updated upstream
-    public String getSenha() {
-        return senha;
-=======
+
     public void setEmail(String email) {
         this.email = email;
->>>>>>> Stashed changes
+
     }
 
     public boolean isStatus() {
         return status;
     }
 
-<<<<<<< Updated upstream
+
     public int getCpf() {
         return cpf;
     }
-=======
+
     public void setStatus(boolean status) {
         this.status = status;
     }
 
-    public int getCpf() {
-        return cpf;
-    }
 
     public void setCpf(int cpf) {
         this.cpf = cpf;
     }
->>>>>>> Stashed changes
+
 }
+
