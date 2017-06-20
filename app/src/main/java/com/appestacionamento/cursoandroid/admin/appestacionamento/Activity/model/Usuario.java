0a715f8cbc62289a631c75ed3a82fa5503bc1b6e
BrunoAsnt <bruno20152013@gmail.com>
2017-06-20 08:34:03 -0300

    Classe Usuario

--- app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/model/Usuario.java
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/model/Usuario.java
@@ -4,14 +4,20 @@ package com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.mode
 import com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.Application.configuracaoFirebase;
 import com.google.firebase.database.DatabaseReference;
 
-public class Usuario extends Pessoa{
-
+public class Usuario {
+    private String nome, uid, telefone, email, tipo;
+    private final String senha = "200200";
+    private boolean status;
+    private int cpf;
     private DatabaseReference firebaseReferences;
 
-    private String tipo;
-
     public Usuario(String nome, String uid, String telefone, String email, boolean status, int cpf, String tipo) {
-        super(nome, uid, telefone, email, status, cpf);
+        this.nome = nome;
+        this.uid = uid;
+        this.telefone = telefone;
+        this.email = email;
+        this.status = status;
+        this.cpf = cpf;
         this.tipo = tipo;
         firebaseReferences = configuracaoFirebase.getFirebase();
     }
@@ -20,9 +26,31 @@ public class Usuario extends Pessoa{
         firebaseReferences.child("users").child(getUid()).setValue(this);
     }
 
-    public String getTipo() {
-        return tipo;
+    public String getNome() {
+        return nome;
     }
 
+    public String getUid() {
+        return uid;
+    }
 
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
 }
