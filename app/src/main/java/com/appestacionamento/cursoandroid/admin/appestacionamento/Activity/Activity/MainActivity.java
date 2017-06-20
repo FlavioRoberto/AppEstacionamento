e4bf7c310300253089301dc842790469c7b6284a
BrunoAsnt <bruno20152013@gmail.com>
2017-06-20 11:18:55 -0300

    Autenticacao de Usuarios

--- app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Activity/MainActivity.java
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,9 +11,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
         //Teste
         //Teste Flavio
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
     }
 }
