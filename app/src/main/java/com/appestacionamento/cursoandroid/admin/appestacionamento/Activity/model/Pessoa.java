de919d874ecf27864d913da39246b950153e1bf7
FlavioRoberto <flaviolive3@hotmail.com>
2017-06-20 08:35:58 -0300

    Estrutura android MvP

--- app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/model/Pessoa.java
+++
@@ -1,8 +0,0 @@
-package com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.model;
-
-
-
-public class Pessoa {
-    private String nome, uid;
-    private final String senha = "200200";
-}
