78a316697fffb0c68e896c9077da0f03c76f513d
BrunoAsnt <bruno20152013@gmail.com>
2017-06-20 08:20:09 -0300

    Modificação Classe Usuario

--- app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/model/Usuario.java
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/model/Usuario.java
@@ -1,6 +1,28 @@
 package com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.model;
 
 
+import com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.Application.configuracaoFirebase;
+import com.google.firebase.database.DatabaseReference;
 
 public class Usuario extends Pessoa{
+
+    private DatabaseReference firebaseReferences;
+
+    private String tipo;
+
+    public Usuario(String nome, String uid, String telefone, String email, boolean status, int cpf, String tipo) {
+        super(nome, uid, telefone, email, status, cpf);
+        this.tipo = tipo;
+        firebaseReferences = configuracaoFirebase.getFirebase();
+    }
+
+    public void create(){
+        firebaseReferences.child("usuarios").child(getUid()).setValue(this);
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+
 }
