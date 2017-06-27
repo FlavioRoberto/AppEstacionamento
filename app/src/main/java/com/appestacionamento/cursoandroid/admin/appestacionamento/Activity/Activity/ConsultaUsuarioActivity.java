717209b1e6d18347571771371ea9dca5c9ce4e36
FlavioRoberto <flaviolive3@hotmail.com>
2017-06-27 16:59:41 -0300

    chamada das telas feitas

--- app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Activity/ConsultaUsuarioActivity.java
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Activity/ConsultaUsuarioActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.Model.Usuario;
 import com.appestacionamento.cursoandroid.admin.appestacionamento.R;
 
 public class ConsultaUsuarioActivity extends AppCompatActivity implements IActivity{
@@ -20,14 +21,17 @@ public class ConsultaUsuarioActivity extends AppCompatActivity implements IActiv
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_consulta_usuario);
+
         //invocacao do toolbar
 
         toolbar = (Toolbar)findViewById(R.id.toolbarId);
-        toolbar.setTitle("Consulta de usuário");
+        toolbar.setTitle("Consulta de Usuário");
+
         setSupportActionBar(toolbar);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_consulta_usuario);
+
 
         btnCadastrar = (Button)findViewById(R.id.btnCadastrar);
 
@@ -43,36 +47,41 @@ public class ConsultaUsuarioActivity extends AppCompatActivity implements IActiv
 
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case  R.id.menu_anterior: voltar();break;
-            case R.id.menu_sair:sair();break;
-            default: return true;
+            case R.id.menu_anterior: voltar();break;
+            case R.id.menu_meusdados: break;
+            case R.id.menu_sair: sair();break;
+            default:break;
         }
         return true;
     }
 
+
+    //invoca os itens no menu
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_admin,menu);
         return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
-    public void sair() {
-       Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+
+
+    //desloga usuario e vai pra tela de login
+    public void sair(){
+        Usuario usuario = new Usuario();
+        usuario.desloga();
+        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
         startActivity(intent);
         finish();
     }
 
-    @Override
-    public void voltar() {
+    //retorna para a página inicial
+    public  void voltar(){
         Intent intent = new Intent(getApplicationContext(),AdmActivity.class);
         startActivity(intent);
         finish();
-
     }
 }
