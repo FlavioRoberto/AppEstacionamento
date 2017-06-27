5c662e33432144d7932b682f111d37d6cac63745
FlavioRoberto <flaviolive3@hotmail.com>
2017-06-27 16:33:29 -0300

    chamada das telas feitas

--- app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Activity/ConsultaUsuarioActivity.java
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Activity/ConsultaUsuarioActivity.java
@@ -1,12 +1,16 @@
 package com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.appestacionamento.cursoandroid.admin.appestacionamento.R;
 
 public class ConsultaUsuarioActivity extends AppCompatActivity {
 
+        private Button btnCadastrar;
 
 
     @Override
@@ -14,6 +18,19 @@ public class ConsultaUsuarioActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_consulta_usuario);
+
+        btnCadastrar = (Button)findViewById(R.id.btnCadastrar);
+
+        //ao clicar no btnCadastrar chama tela cadastrar
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CadastroUsuarioActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
 
