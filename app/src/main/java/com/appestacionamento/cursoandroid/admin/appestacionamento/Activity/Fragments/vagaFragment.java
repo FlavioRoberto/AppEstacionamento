4d3eeabba77377f9a46926275591fb9284344548
FlavioRoberto <flaviolive3@hotmail.com>
2017-06-27 10:55:10 -0300

    menu vaga completo

--- app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Fragments/vagaFragment.java
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Fragments/vagaFragment.java
@@ -24,7 +24,7 @@ public class vagaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View view = inflater.inflate(R.layout.fragment_consulta_usuario, container, false);
+       View view = inflater.inflate(R.layout.fragment_vaga, container, false);
         return view;
     }
 
