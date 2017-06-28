19995f4c9d6f50d26ce672ea84bd8c3e2fece55e
StrazCree <mateusgraciano53@gmail.com>
2017-06-27 23:03:06 -0300

    exclusao veículo layout Atualizado 1_0

--- app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Activity/ExcluirUsuarioActivity.java
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Activity/ExcluirUsuarioActivity.java
@@ -26,7 +26,7 @@ public class ExcluirUsuarioActivity extends AppCompatActivity implements IActivi
 
 
 
-    //ao clicar nas opções do menu seleciona uma ação
+    //ao clicar nas opções do menu seleciona uma açãoo
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
