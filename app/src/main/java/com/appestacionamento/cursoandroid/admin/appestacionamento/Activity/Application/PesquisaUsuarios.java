55387dde2afbcc87a27240547b9c1cc2641fe767
BrunoAsnt <bruno20152013@gmail.com>
2017-07-04 00:56:22 -0300

    Merge branch 'master' of https://github.com/FlavioRoberto/AppEstacionamento
    Metodo Pesquisa Usuario

---
+++ app/src/main/java/com/appestacionamento/cursoandroid/admin/appestacionamento/Activity/Application/PesquisaUsuarios.java
@@ -0,0 +1,50 @@
+package com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.Application;
+
+import com.appestacionamento.cursoandroid.admin.appestacionamento.Activity.Helper.Base64Custom;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class PesquisaUsuarios {
+
+    private String emailCodificado, emailDatabase;
+    private Boolean flag = false;
+    private DatabaseReference databaseReferenceUsers = FirebaseDatabase.getInstance().getReference("users");
+
+
+    public PesquisaUsuarios() {
+    }
+
+    //Método que verifica se o email passado é valido
+    public boolean retornaUsuario(String email){
+        emailCodificado = Base64Custom.codificarBase64(email);
+        Query query = databaseReferenceUsers;
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    emailDatabase = postSnapshot.child("uid").getValue(String.class);
+                    try{
+                        if(emailDatabase.equals(emailCodificado)){
+                            flag = true;
+                            break;
+                        }
+                    }catch (Exception e){
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return flag;
+    }
+    //Fim do metodo
+
+}
